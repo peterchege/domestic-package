b1fc44e9b99eb78306c19dceecbb65b6d53e577f
anthony-baru <anthonybaru@gmail.com>
2019-09-05 11:40:41 +0300

    feeding data using ajax complete

--- app/Http/Controllers/CalculatorController.php
+++ app/Http/Controllers/CalculatorController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Pr_dp_calculator_customer_quotation;
 use Illuminate\Http\Request;
 
 class CalculatorController extends Controller
@@ -23,6 +24,19 @@ class CalculatorController extends Controller
      */
     public function ajaxRequestPost(Request $request)
     {
+        $feed = new Pr_dp_calculator_customer_quotation;
+        $feed->full_name = $request->input('name');
+        $feed->phone_number = $request->input('phone');
+        $feed->email = $request->input('email');
+        $feed->location = $request->input('location');
+        $feed->property = $request->input('property');
+        $feed->premises = $request->input('building_amount');
+        $feed->contents = $request->input('content_amount');
+        $feed->all_risk = $request->input('risk_amount');
+        $feed->domestic_employee = $request->input('domestic_amount');
+        $feed->liability = $request->input('liability_amount');
+        $feed->save();
+
         $input = $request->all();
         return response()->json(['success' => $input]);
     }
